cbf93aa082fe0efe4caaa8cba0124c6d060b9714
Pablo Vieyra <vieyrapez@gmail.com>
2022-03-10 13:29:44 -0600

    Cambio de color btn Crear Contactos

--- resources/views/livewire/create.blade.php
+++ resources/views/livewire/create.blade.php
@@ -16,7 +16,7 @@
     </div>
     <div class="w-auto pl-3 text-right">
       <div class="pt-5">
-        <button wire:click="store()" class="px-3 py-2 bg-purple-200 text-purple-500 hover:bg-purple-500 hover:text-purple-100 rounded">Agregar contacto</button>
+        <button wire:click="store()" class="px-3 py-2 bg-blue-200 text-blue-500 hover:bg-blue-500 hover:text-blue-100 rounded">Agregar contacto</button>
       </div>
     </div>
   </div>
