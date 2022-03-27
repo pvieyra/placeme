16abdf62064e7c7ad0c6685260469f2df39fe173
Pablo Vieyra <vieyrapez@gmail.com>
2022-03-26 19:53:37 -0600

    Se configuran botones en la vista Ver usuarios: pdf, excel, paginacion

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -10,6 +10,12 @@
   <link href="{{ asset('css/alpha.min.css')}}" rel="stylesheet" type="text/css"/>
   <link href="{{ asset('css/custom.css')}}" rel="stylesheet" type="text/css"/>
   <link href="{{ asset('plugins/datatables/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+  <link href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet">
+  <link href="https://cdn.datatables.net/1.11.5/css/dataTables.jqueryui.min.css" rel="stylesheet">
+  <link href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.jqueryui.min.css" rel="stylesheet">
+
+
+
 @endsection
 
 @section('content')
@@ -17,7 +23,7 @@
     <div class="card-content">
       <span class="card-title">Creación de usuarios</span><br>
       <div class="row">
-        <table id="example" class="table table-striped table-bordered  mt-4 responsive-table">
+        <table id="example" class="display responsive-table" style="width: 100%">
           <thead>
           <tr>
             <th>ID </th>
@@ -38,22 +44,45 @@
   <script src="{{ asset('plugins/materialize/js/materialize.min.js')}}"></script>
   <script src="{{ asset('plugins/material-preloader/js/materialPreloader.min.js')}}"></script>
   <script src="{{ asset('plugins/jquery-blockui/jquery.blockui.js')}}"></script>
-  <script src="{{ asset('plugins/datatables/js/jquery.dataTables.min.js')}}"></script>
+  <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.11.5/js/dataTables.jqueryui.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.jqueryui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.colVis.min.js"></script>
   <script src="{{ asset('js/alpha.min.js')}}"></script>
-  <script src="{{ asset('js/pages/form_elements.js')}}"></script>
   <script>
       $(document).ready(function() {
-          $('#example').DataTable({
+          var table = $('#example').DataTable({
               processing: true,
               serverSide: true,
               responsive:true,
+              autoWidth: false,
+              lengthChange: false,
+              dom:'Bfrtip',
+              lengthMenu:[
+                  [10, 25, 50, 100],
+                  ['10 filas', '25 filas', '50 filas', '100 filas'],
+              ],
               buttons: [
                   {
-                      extend: 'copy',
-                      text: 'Copiado a portapapeles',
+                      extend:'pageLength',
+                      text: 'Paginas',
+                  },
+                  {
+                    extend:'copy',
+                    text:'Copiar',
                   },
                   'excel',
-                  'pdf'
+                  'pdf',
+                  {
+                      extend: 'print',
+                      text:'Imprimir',
+                  }
               ],
               "language": {
                   "searchPlaceholder": 'Nombre, Correo',
@@ -64,12 +93,12 @@
                   "infoEmpty": "Sin registros disponibles",
                   "infoFiltered" : "(filtrado de _MAX_ registros totales)",
                   sLength: 'dataTables_length',
-                  oPaginate: {
-                      sFirst: '<i class="material-icons">chevron_left</i>',
-                      sPrevious: '<i class="material-icons">chevron_left</i>',
-                      sNext: '<i class="material-icons">chevron_right</i>',
-                      sLast: '<i class="material-icons">chevron_right</i>'
-                  }
+                  // oPaginate: {
+                  //     sFirst: '<i class="material-icons">chevron_left</i>',
+                  //     sPrevious: '<i class="material-icons">chevron_left</i>',
+                  //     sNext: '<i class="material-icons">chevron_right</i>',
+                  //     sLast: '<i class="material-icons">chevron_right</i>'
+                  // }
               },
               //mandar ruta de laravel
               "ajax":"{{ route('datatable.users')}}",
@@ -80,6 +109,8 @@
                   { data:'action',orderable: false, searchable: false},
               ]
           });
+
+
           $('.dataTables_length select').addClass('browser-default');
       });
   </script>
